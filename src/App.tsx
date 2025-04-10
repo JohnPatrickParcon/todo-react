70a8dfdfa308586e53e8cfb1742279c082f66be8
John Patrick Parcon <johnpatrickparcon@gmail.com>
2025-04-11 04:59:58 +0800

    add delete

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,43 @@ type TodoItemType = {
 
 function App() {
   const [todoList, setTodoList] = useState<TodoItemType[]>([])
-  const [todo, setTodo] = useState<string>()
 
-  function submitHandler(e: FormEvent) {
+  function submitHandler(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
-
-    if(!todo){
+    
+    const formData = new FormData(e.currentTarget)
+    const todo = formData.get('todo') as string | null
+    
+    if (!todo) {
       return null
     }
+
     setTodoList([...todoList, {id: Date.now().toString(), todoItem: todo}])
-    setTodo('')
+    e.currentTarget.reset()
   }
 
+  function removeTodo(id: string){
+    const filteredTodo = todoList.filter(item => id !== item.id)
+
+    setTodoList(filteredTodo)
+  }
+
+  function RenderTodo(){
+    return(
+      <ul className="grid w-1/2 bg-purple-600 rounded-2xl mt-2 p-1 overflow-y-auto">
+        {
+          todoList.map(item => {
+            return(
+              <li className='w-full wrap-anywhere p-1 flex place-content-between' key={item.id}>
+              {item.todoItem}
+              <button className="bg-red-400 rounded-2xl p-1 hover:bg-red-500" onClick={() => {removeTodo(item.id)}}>Delete</button>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
   return (
     <>
       <div className="flex flex-col items-center p-4 bg-purple-300 w-full h-dvh">
@@ -28,14 +53,11 @@ function App() {
             className="bg-purple-400 p-2 rounded-l-xl"
             placeholder="What to do?"
             required
-            value={todo}
-            onChange={e => {setTodo(e.target.value)}}></input>
+            name="todo"></input>
           <button type="submit" className=" justify-center bg-purple-500 p-2 rounded-r-xl">Add Todo</button>
         </form>
 
-        <ul>
-          {todoList.map(item => <li key={item.id}>{item.todoItem}</li>)}
-        </ul>
+        <RenderTodo/>
       </div>
     </>
   )
